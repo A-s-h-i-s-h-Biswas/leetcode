b2a317f869a7861aca38cd4f94db6859b3ff59af
Ashish Biswas <107340046+A-s-h-i-s-h-Biswas@users.noreply.github.com>
2023-02-05 10:45:33 +0530

    Update 0083-remove-duplicates-from-sorted-list.java

--- java/0083-remove-duplicates-from-sorted-list.java
+++ java/0083-remove-duplicates-from-sorted-list.java
@@ -1,20 +1,18 @@
-//Three pointer approach that we use in in-place reversal of linked list.
+//Two pointer approach that we use in in-place reversal of linked list.
 
 class Solution {
 
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode p = null;
-        ListNode q = null;
-        ListNode r = head;
-        while (r != null) {
-            if (q != null && q.val == r.val) {
-                r = r.next;
-                q.next = r;
-            } else {
-                p = q;
-                q = r;
-                r = r.next;
+        ListNode node = null;
+        ListNode curr = head;
+        while (curr != null) {
+            if(node!=null && node.val==curr.val){
+                node.next=curr.next;
             }
+            else{
+                node=curr;
+            }
+            curr=curr.next;
         }
         return head;
     }
